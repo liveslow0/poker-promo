30898c0b52815920e5aa5e00f5f57b0556821879
liveslow0 <around.the.globe03@gmail.com>
2025-05-14 22:31:54 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},  // ✅ required for Tailwind v4+
-    autoprefixer: {}
-  }
-}
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
